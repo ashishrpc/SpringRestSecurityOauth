da80c66f7db4d3a76fbe4d3f33a01eba5e488587
ashish.prasad <ashish.prasad@KMG-016.kmg.com>
2017-08-21 15:28:48 +0530

    security

---
+++ SpringRestSecurityOauth/src/main/java/com/dragonx/controllers/RestController.java
@@ -0,0 +1,28 @@
+package com.dragonx.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.dragonx.models.User;
+import com.dragonx.services.DataService;
+
+
+@Controller
+@RequestMapping("/api/users")
+public class RestController {
+
+	@Autowired
+	DataService dataService;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
+	public List<User> list() {
+		return dataService.getUserList();
+
+	}
+}
---
+++ SpringRestSecurityOauth/src/main/java/com/dragonx/services/DataService.java
@@ -0,0 +1,9 @@
+package com.dragonx.services;
+
+import java.util.List;
+
+import com.dragonx.models.User;
+
+public interface DataService {
+	public List<User> getUserList();
+}
